964f44c90d6835b1c20583bb06dae59614d6a6af
xendan <mukhametova@gmail.com>
2019-06-04 19:58:05 -0700

    added apm dependency

--- src/main/java/org/pipelineexample/apm/PipelineProcessor.java
+++ src/main/java/org/pipelineexample/apm/PipelineProcessor.java
@@ -1,15 +1,22 @@
 package org.pipelineexample.apm;
 
+import co.elastic.apm.api.ElasticApm;
+import co.elastic.apm.api.Span;
+import co.elastic.apm.api.Transaction;
+
 import java.io.*;
 import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.Charset;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PipelineProcessor {
-    public static final String LOCALHOST = "127.0.0.1";
+
+    private static final String LOCALHOST = "127.0.0.1";
+    private static final Pattern TRANS_ID_PATTERN = Pattern.compile("PID:(.+);");
+
     private final String name;
     private final int inPort;
     private final int outPort;
@@ -22,21 +29,72 @@ public class PipelineProcessor {
 
     public void process() throws InterruptedException, IOException {
         info("started");
-        String message = readInput();
+        String inMessage = readInput();
+        String outMessage = processMessage(inMessage);
+        sendMessage(outMessage);
+        info("Bye");
+    }
+
+    private String processMessage(String message) throws InterruptedException {
+        Transaction parentTransaction = getOrCreateTransaction(extractId(message));
+        Span span = parentTransaction.createSpan();
+        try {
+            span.setName(name);
+            thisIsAcutallyABusinessLogic(message);
+            return  injectParentTransactionId(message, parentTransaction) + ", processed by " + name;
+        } catch (Exception e) {
+            parentTransaction.captureException(e);
+            span.captureException(e);
+            throw e;
+        } finally {
+            span.end();
+            if ("Sink".equals(name)) {
+                parentTransaction.end();
+            }
+        }
+    }
+
+    private String extractId(String message) {
+        Matcher matcher = TRANS_ID_PATTERN.matcher(message);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    private void thisIsAcutallyABusinessLogic(String message) throws InterruptedException {
         info("processing message \"" + message + "\"");
         TimeUnit.SECONDS.sleep(3);
-        String newMessage = message + ", processed by " + name;
-        if (outPort != -1) {
-            info("sending:\"" + newMessage + "\" to port " + outPort);
+    }
+
+    private String injectParentTransactionId(String message, Transaction parentTransaction) {
+        Matcher matcher = TRANS_ID_PATTERN.matcher(message);
+        if (matcher.find()) {
+            return message;
         }
-        sendMessage(newMessage);
-        info("Bye");
+        String id = parentTransaction.getId();
+        if (id.isEmpty()) {
+            id = "111111111";
+        }
+        return message + " PID:" + id + ";";
+    }
+
+    private Transaction getOrCreateTransaction(String transactionId) {
+        Transaction transaction;
+        if ("Source".equals(name)) {
+            transaction = ElasticApm.startTransaction();
+        } else {
+            transaction = ElasticApm.startTransactionWithRemoteParent(key -> transactionId);
+        }
+        transaction.setName("apmExamplePipeline");
+        return transaction;
     }
 
     private void sendMessage(String message) throws IOException {
         if (outPort == -1) {
             info("Pipeline ended!");
         } else {
+            info("sending : \"" + message + "\" to port " + outPort);
             ServerSocket serverSocket = new ServerSocket(outPort);
             Socket socket = serverSocket.accept();
             OutputStream os = socket.getOutputStream();
