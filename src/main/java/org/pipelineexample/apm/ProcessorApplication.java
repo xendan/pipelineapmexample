60edf153493cb22ef8414fdfcf8b5da1b2913fa7
xendan <mukhametova@gmail.com>
2019-06-07 17:01:17 -0700

    better jar API created

--- src/main/java/org/pipelineexample/apm/ProcessorApplication.java
+++ src/main/java/org/pipelineexample/apm/ProcessorApplication.java
@@ -4,18 +4,19 @@ import org.pipelineexample.apm.processor.PipelineProcessor;
 import org.pipelineexample.apm.processor.PipelineProcessorFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
 
-/**
- * Builds and run processor which properties is defined by arguments and 'pipeline.properties'
- */
 public class ProcessorApplication {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Expect 2 arguments: number of processor and comma separated list of ports");
+        System.out.println(Arrays.toString(args));
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Expect min 3 arguments: name, inPort, outPort and optional burnCpus");
         }
-        boolean burnCpus = args.length > 2 && Boolean.valueOf(args[2]);
-        PipelineProcessor pipelineProcessor = new PipelineProcessorFactory(args[1]).buildPipelineProcessor(args[0], burnCpus);
+        boolean burnCpus = args.length > 3 && Boolean.valueOf(args[3]);
+        int inPort = Integer.parseInt(args[1]);
+        int outPort = Integer.parseInt(args[2]);
+        PipelineProcessor pipelineProcessor = new PipelineProcessorFactory().buildPipelineProcessor(args[0], inPort, outPort, burnCpus);
         pipelineProcessor.process();
     }
 
--- src/main/java/org/pipelineexample/apm/processor/PipelineProcessorFactory.java
+++ src/main/java/org/pipelineexample/apm/processor/PipelineProcessorFactory.java
@@ -9,60 +9,18 @@ import java.util.stream.Stream;
 
 public class PipelineProcessorFactory {
 
-    private final List<Integer> ports;
-
-    public PipelineProcessorFactory(String portsString) {
-        this(portsString.split(","));
-    }
-
-    public PipelineProcessorFactory(String[] ports) {
-        this(Stream.of(ports).map(Integer::valueOf).collect(Collectors.toList()));
-    }
-
-    public PipelineProcessorFactory(List<Integer> ports) {
-        if (ports.size() <= 2) {
-            throw new IllegalArgumentException("Number of ports must be > 2");
-        }
-        this.ports = ports;
-    }
-
-    public PipelineProcessor buildPipelineProcessor(String arg, boolean burnCpus) {
-        Integer currentNumber = Integer.valueOf(arg);
-        ProcessorType type = getType(currentNumber);
-        String name = getName(currentNumber, type);
-        int inPort = getPort(currentNumber, true);
-        int outPort = getPort(currentNumber, false);
+    public PipelineProcessor buildPipelineProcessor(String name, int inPort, int outPort, boolean burnCpus) {
+        ProcessorType type = getType(inPort, outPort);
         InfoConsole infoConsole = new InfoConsole(name);
         LowBudgetKafka kafka = new LowBudgetKafka(inPort, outPort, infoConsole, burnCpus);
         return new PipelineProcessor(name, kafka, type);
     }
 
-    private int getPort(Integer currentNumber, boolean isIn) {
-        if ((currentNumber == 1 && isIn) || (currentNumber.equals(ports.size() + 1) && !isIn)) {
-            return -1;
-        }
-        int index = isIn ? currentNumber - 2 : currentNumber - 1;
-        return ports.get(index);
-    }
-
-    private String getName(Integer currentNumber, ProcessorType type) {
-        switch (type) {
-            case SINK:
-                return "Sink";
-            case SOURCE:
-                return "Source";
-            case PRCOSSOR:
-                return "Processor-" + (currentNumber - 1);
-            default:
-                throw new IllegalArgumentException("Unsupported type " + type);
-        }
-    }
-
-    private ProcessorType getType(Integer currentNumber) {
-        if (1 == currentNumber) {
+    private ProcessorType getType(int inPort, int outPort) {
+        if (-1 == inPort) {
             return ProcessorType.SOURCE;
         }
-        if (currentNumber.equals(ports.size() + 1)) {
+        if (-1 == outPort) {
             return ProcessorType.SINK;
         }
         return ProcessorType.PRCOSSOR;
